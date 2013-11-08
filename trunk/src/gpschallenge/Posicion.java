b8023b220ed354f0ae8f5bb29660a5e1926b3c56
delpinor <delpinor@98c15dae-8109-986d-03b3-e374b46faa3a>
2013-11-08 05:05:49 +0000

    Se agrego la clase Posicion, PosicionTest

---
+++ trunk/src/gpschallenge/Posicion.java
@@ -0,0 +1,28 @@
+package gpschallenge;
+
+public class Posicion {
+	private int x = 0;
+	private int y = 0;
+	public Posicion(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	public void setX(int x){
+		this.x = x;
+	}
+	public void setY(int y){
+		this.y = y;
+	}
+	public int getX(){
+		return this.x;
+	}
+	public int getY(){
+		return this.y;
+	}
+	public boolean esIgual(Posicion p){
+		if((p.getX() == this.x) && (p.getY() == this.y))
+			return true;
+		return false;
+	}
+
+}
