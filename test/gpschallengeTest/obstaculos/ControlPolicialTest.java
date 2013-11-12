bf0124a68ba4e509401ea2a8ab1716f62e96c8d1
delpinor <delpinor@98c15dae-8109-986d-03b3-e374b46faa3a>
2013-11-12 02:53:35 +0000

    Subo pruebas de ControlPolicialTest.java

--- test/gpschallengeTest/obstaculos/ControlPolicialTest.java
+++ test/gpschallengeTest/obstaculos/ControlPolicialTest.java
@@ -1,9 +1,11 @@
 package gpschallengeTest.obstaculos;
 
 import static org.junit.Assert.*;
+
 import gpschallenge.componentes.obstaculos.ControlPolicial;
 import gpschallenge.componentes.vehiculos.Auto;
 import gpschallenge.componentes.vehiculos.CuatroXCuatro;
+import gpschallenge.componentes.vehiculos.EstadoVehiculo;
 import gpschallenge.componentes.vehiculos.Moto;
 
 import org.junit.Before;
@@ -19,6 +21,21 @@ public class ControlPolicialTest {
 	private Moto unaMoto = null;
 	private Auto unAuto = null;
 	private CuatroXCuatro una4x4 = null;
+	public double porcentajeDeAparicion(EstadoVehiculo unVehiculo){
+		double cantidad = 0;
+		/* Pruebo 1000 casos y calculo la probabilidad de ser penalizado */
+		for(int i = 0; i < 1000; i++){
+			unVehiculo.sumarMovimientos(5);
+			unVehiculo.afectar(unControlPolicial);
+			if(unVehiculo.getCantMovimientos() == 8){
+				cantidad += 1;
+			}
+			unVehiculo.reiniciarValoresACero();
+		}
+		double valor = cantidad/1000;
+		valor = Math.rint(valor*10)/10;
+		return valor;
+	}
 	@Before
 	public void setUp(){
 		unControlPolicial = new ControlPolicial();
@@ -30,21 +47,15 @@ public class ControlPolicialTest {
 		unAuto.reiniciarValoresACero();
 	}
 	@Test
-	public void debePenalizarAMoto(){
-		unaMoto.sumarMovimientos(5);
-		unControlPolicial.afectarMoto(unaMoto);
-		assertEquals(unaMoto.getCantMovimientos(), 8);
+	public void probabilidadDeSerPenalizadoAuto(){
+		assertTrue(this.porcentajeDeAparicion(unAuto) == 0.5);
 	}
 	@Test
-	public void debePenalizarAuto(){
-		unAuto.sumarMovimientos(8);
-		unAuto.afectar(unControlPolicial);
-		assertEquals(unAuto.getCantMovimientos(), 11);
+	public void probabilidadDeSerPenalizadoMoto(){
+		assertTrue(this.porcentajeDeAparicion(unaMoto) == 0.8);
 	}
 	@Test
-	public void debePenalizarCuatroXCuatro(){
-		una4x4.sumarMovimientos(10);
-		una4x4.afectar(unControlPolicial);
-		assertEquals(una4x4.getCantMovimientos(), 13);
+	public void probabilidadDeSerPenalizadoCuatroXCuatro(){
+		assertTrue(this.porcentajeDeAparicion(una4x4) == 0.3);
 	}
 }
