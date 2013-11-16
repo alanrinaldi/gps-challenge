452778df5364ec9a05f7f469d66be374ff303cf0
Apredazzi@gmail.com <Apredazzi@gmail.com@98c15dae-8109-986d-03b3-e374b46faa3a>
2013-11-16 23:44:52 +0000

    Agregue 2 pruebas que verifican que se mueva bien y que aplique bien las sorpresas

--- trunk/test/gpschallengeTest/mapa/EsquinaTest.java
+++ trunk/test/gpschallengeTest/mapa/EsquinaTest.java
@@ -1,7 +1,13 @@
 package gpschallengeTest.mapa;
 
 import static org.junit.Assert.*;
+import gpschallenge.componentes.obstaculos.Pozo;
 import gpschallenge.componentes.utililidades.Posicion;
+import gpschallenge.componentes.vehiculos.Auto;
+import gpschallenge.componentes.vehiculos.EstadoVehiculo;
+import gpschallenge.componentes.vehiculos.Vehiculo;
+import gpschallenge.direccionamiento.*;
+import gpschallenge.mapa.Calle;
 import gpschallenge.mapa.Esquina;
 
 import org.junit.Test;
@@ -16,5 +22,66 @@ public class EsquinaTest {
 		assertEquals(esquina.getPosicion(),unaPosicion);
 		
 	}
+	
+	@Test
+	public void deberiaMoverseALaSiguienteEsquina(){
+		
+		Posicion unaPosicion = new Posicion(1,1);
+		Posicion otraPosicion = new Posicion(1,3);
+		Esquina unaEsquina = new Esquina(unaPosicion);
+		Esquina otraEsquina = new Esquina(otraPosicion);
+		
+		Calle unaCalle = new Calle(unaEsquina,otraEsquina);
+		Direccion arriba = new Arriba();
+		Direccion abajo = new Abajo();
+		unaEsquina.agregarCalle(arriba , unaCalle);
+		otraEsquina.agregarCalle(abajo, unaCalle);
+		
+		EstadoVehiculo auto = Auto.getInstancia();
+		Vehiculo vehiculo = new Vehiculo(auto);
+		
+		vehiculo.setPosicion(unaEsquina);
+		
+		assertEquals(unaEsquina.getPosicion(),vehiculo.getPosicionActual().getPosicion());
+		
+		vehiculo.moverA(arriba);
+		
+		assertEquals(otraEsquina.getPosicion(),vehiculo.getPosicionActual().getPosicion());
+		assertEquals(1,vehiculo.getCantMovimientos());
+		
+		
+	}
+	
+	@Test
+	public void deberiaMoverseALaSiguienteEsquinaYAplicarObstaculo(){
+		
+		Posicion unaPosicion = new Posicion(1,1);
+		Posicion otraPosicion = new Posicion(1,3);
+		Esquina unaEsquina = new Esquina(unaPosicion);
+		Esquina otraEsquina = new Esquina(otraPosicion);
+		
+		Pozo unPozo = new Pozo();
+		Calle unaCalle = new Calle(unaEsquina,otraEsquina);
+		unaCalle.setAfectable(unPozo);
+		Direccion arriba = new Arriba();
+		Direccion abajo = new Abajo();
+		unaEsquina.agregarCalle(arriba , unaCalle);
+		otraEsquina.agregarCalle(abajo, unaCalle);
+		
+		EstadoVehiculo auto = Auto.getInstancia();
+		Vehiculo vehiculo = new Vehiculo(auto);
+		
+		vehiculo.setPosicion(unaEsquina);
+		
+		assertEquals(unaEsquina.getPosicion(),vehiculo.getPosicionActual().getPosicion());
+		
+		vehiculo.moverA(arriba);
+		
+		assertEquals(otraEsquina.getPosicion(),vehiculo.getPosicionActual().getPosicion());
+		assertEquals(4,vehiculo.getCantMovimientos());
+		
+		
+	}
+
 
 }
