9bfa9e5eeeea6b3a56a948212390bb44220cfdce
delpinor <delpinor@98c15dae-8109-986d-03b3-e374b46faa3a>
2013-11-17 04:18:25 +0000

    Se suben de la clase Vehiculo

--- trunk/test/gpschallengeTest/vehiculos/VehiculoTest.java
+++ trunk/test/gpschallengeTest/vehiculos/VehiculoTest.java
@@ -1,17 +1,24 @@
 package gpschallengeTest.vehiculos;
 
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import gpschallenge.componentes.obstaculos.*;
+import gpschallenge.componentes.utililidades.Posicion;
 import gpschallenge.componentes.vehiculos.Auto;
+import gpschallenge.componentes.vehiculos.CuatroXCuatro;
 import gpschallenge.componentes.vehiculos.EstadoVehiculo;
 import gpschallenge.componentes.vehiculos.Moto;
 import gpschallenge.componentes.vehiculos.Vehiculo;
+import gpschallenge.mapa.Esquina;
 
 import org.junit.Test;
 
 public class VehiculoTest {
-	
+	private ArrayList<Afectable> afectables = new ArrayList<Afectable>();
+	private Vehiculo unVehiculo = new Vehiculo();
 	@Test
 	public void deberiaInicializarVehiculo(){
 		
@@ -48,6 +55,41 @@ public class VehiculoTest {
 		
 		assertEquals(moto,vehiculo.getEstado());
 		
+	}
+	@Test
+	public void vehiculoConEstadoAutofectadoPorDosObstaculos(){
+		unVehiculo.setEstado(Auto.getInstancia());
+		unVehiculo.sumarMovimientos(10);
+		afectables.add(new Pozo()); // suma 3 movimientos
+		afectables.add(new Piquete()); // vuelve a posicion anterior
+		Esquina unaEsquina = new Esquina(new Posicion(10, 10));
+		unVehiculo.setPosicion(unaEsquina);
+		unVehiculo.afectar(afectables);
+		assertTrue(unVehiculo.getPosicionActual().getPosicion().esIgual(new Posicion(0, 0)));
+		assertEquals(unVehiculo.getCantMovimientos(), 13);
+		
+	}
+	@Test
+	public void vehiculoConEstadoMotofectadoPorDosObstaculos(){
+		unVehiculo.setEstado(Moto.getInstancia());
+		unVehiculo.sumarMovimientos(10);
+		afectables.add(new Pozo()); // suma 3 movimientos
+		afectables.add(new Piquete()); // Suma 2 movimientos
+		unVehiculo.afectar(afectables);
+		assertEquals(unVehiculo.getCantMovimientos(), 15);
+		
+	}
+	@Test
+	public void vehiculoConEstadoCuatroXCuatrofectadoPorDosObstaculos(){
+		unVehiculo.setEstado(CuatroXCuatro.getInstancia());
+		unVehiculo.sumarMovimientos(10);
+		afectables.add(new Pozo()); // suma 0 movimientos
+		afectables.add(new Piquete()); // vuelve a posicion anterior
+		Esquina unaEsquina = new Esquina(new Posicion(10, 10));
+		unVehiculo.setPosicion(unaEsquina);
+		unVehiculo.afectar(afectables);
+		assertTrue(unVehiculo.getPosicionActual().getPosicion().esIgual(new Posicion(0, 0)));
+		assertEquals(unVehiculo.getCantMovimientos(), 10);
 	}
 
 }
