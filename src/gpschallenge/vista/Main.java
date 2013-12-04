8b3978cb3ae88ad47a4e8668981014993ceedc2d
delpinor <delpinor@98c15dae-8109-986d-03b3-e374b46faa3a>
2013-12-04 03:32:52 +0000

    Se renombra ventana principal

--- src/gpschallenge/vista/Main.java
+++ src/gpschallenge/vista/Main.java
@@ -1,21 +1,110 @@
 package gpschallenge.vista;
 
-import gpschallenge.componentes.utililidades.Posicion;
+import gpschallenge.excepciones.EsquinasInvalidasException;
+import gpschallenge.motor.Jugador;
 
-public class Main {
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Main extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Posicion pos1 = new Posicion(1, 1);
-		Posicion pos2 = new Posicion(2, 2);
-		Posicion pos3 = new Posicion(3, 3);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("GPS-Challenge");
+		setBounds(0, 0, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.lightGray);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+
+		JButton botonJugar = new JButton("Jugar");
+		botonJugar.setBorder(BorderFactory.createEmptyBorder());
+		botonJugar.setContentAreaFilled(false);
+		// Posicion donde se colocar este boton
+		botonJugar.setBounds(141, 38, 153, 55);
+		botonJugar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Inicia un juego nuevo
+				VentanaConfiguraciones unaVentana = new VentanaConfiguraciones();
+				
+				unaVentana.setVisible(true);
+				dispose();
+
+			}
+		});
+		contentPane.setLayout(null);
+		contentPane.add(botonJugar);
+
+		JButton botonContinuar = new JButton("Continuar");
+		botonContinuar.setBorder(BorderFactory.createEmptyBorder());
+		botonContinuar.setContentAreaFilled(false);
+		// Posicion donde se colocar este boton
+		botonContinuar.setBounds(141, 104, 153, 55);
+		botonContinuar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Carga un juego de partida guardada
+				VentanaContinuacionJuego unaVentana = null;
+				
+				try {
+					unaVentana = new VentanaContinuacionJuego(new Jugador("Pepe"));
+				} catch (EsquinasInvalidasException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+			unaVentana.setVisible(true);
+			dispose();
+			}
+		});
+		contentPane.add(botonContinuar);
+
+		JButton botonSalir = new JButton("Salir");
+		botonSalir.setBorder(BorderFactory.createEmptyBorder());
+		botonSalir.setContentAreaFilled(false);
+		// Posicion donde se colocar este boton
+		botonSalir.setBounds(141, 170, 153, 55);
+		botonSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Sale del programa
+				System.exit(0);
+			}
+		});
+		contentPane.add(botonSalir);
+
 		
-		pos1 = pos2;
-		pos1.sumarPosicion(5, 5);
-		
-		System.out.println(pos1);
-		System.out.println(pos2);
-		System.out.println(pos3);
 	}
 
 }
